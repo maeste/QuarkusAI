85c764acbe5b8606ed16c5999de7bc10f0ab118a
Alessio Soldano <asoldano@redhat.com>
2019-07-21 01:37:58 +0200

    Do not write the file to disk

--- src/main/java/org/acme/quickstart/GreetingResource.java
+++ src/main/java/org/acme/quickstart/GreetingResource.java
@@ -1,18 +1,23 @@
 package org.acme.quickstart;
 
-import org.jboss.resteasy.plugins.providers.multipart.InputPart;
-import org.jboss.resteasy.plugins.providers.multipart.MultipartFormDataInput;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
 
 import javax.inject.Inject;
-import javax.ws.rs.*;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.core.Response;
-import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.List;
-import java.util.Map;
+
+import org.jboss.resteasy.plugins.providers.multipart.InputPart;
+import org.jboss.resteasy.plugins.providers.multipart.MultipartFormDataInput;
 
 @Path("/hello")
 public class GreetingResource {
@@ -35,40 +40,29 @@ public class GreetingResource {
     @POST
     @Path("/labelImage")
     @Consumes("multipart/form-data")
-    public Response loadImage(MultipartFormDataInput input) throws Exception {
-        Map<String, List<InputPart>> formParts = input.getFormDataMap();
-        File f = storeWalletFile(formParts.get("file").iterator().next());
-        //String password = formParts.get("password").iterator().next().getBodyAsString();
-        byte[] bytes = Files.readAllBytes(Paths.get(f.getAbsolutePath()));
+    public Response loadImage(@HeaderParam("Content-Length") String contentLength, MultipartFormDataInput input) throws Exception {
         long before = System.currentTimeMillis();
-        String returnString = (new LabelImage()).labelImage(f.getName(), bytes);
+        InputPart inputPart = input.getFormDataMap().get("file").iterator().next();
+        String fileName = parseFileName(inputPart.getHeaders());
+        byte[] bytes = streamToByte(inputPart.getBody(InputStream.class, null), Integer.parseInt(contentLength));
+        String returnString = (new LabelImage()).labelImage(fileName, bytes);
         return Response.status(200).entity("labeled image = " + returnString + "time=" + (System.currentTimeMillis() - before)).build();
     }
 
     @POST
     @Path("/labelImageStatic")
     @Consumes("multipart/form-data")
-    public Response loadImageStatic(MultipartFormDataInput input) throws Exception {
-        Map<String, List<InputPart>> formParts = input.getFormDataMap();
-        File f = storeWalletFile(formParts.get("file").iterator().next());
-        //String password = formParts.get("password").iterator().next().getBodyAsString();
-        byte[] bytes = Files.readAllBytes(Paths.get(f.getAbsolutePath()));
+    public Response loadImageStatic(@HeaderParam("Content-Length") String contentLength, MultipartFormDataInput input) throws Exception {
         long before = System.currentTimeMillis();
-        String returnString = (new LabelImageStatic()).labelImage(f.getName(), bytes);
+        InputPart inputPart = input.getFormDataMap().get("file").iterator().next();
+        String fileName = parseFileName(inputPart.getHeaders());
+        byte[] bytes = streamToByte(inputPart.getBody(InputStream.class, null), Integer.parseInt(contentLength));
+        String returnString = (new LabelImageStatic()).labelImage(fileName, bytes);
         return Response.status(200).entity("labeled image (static)= " + returnString + "time=" + (System.currentTimeMillis() - before)).build();
     }
 
-    private File storeWalletFile(InputPart inputPart) throws IOException {
-        MultivaluedMap<String, String> headers = inputPart.getHeaders();
-        String fileName = parseFileName(headers);
-        InputStream istream = inputPart.getBody(InputStream.class, null);
-        File f = new File(System.getProperty("java.io.tmpdir"), fileName);
-        saveFile(istream, f);
-        return f;
-    }
-
     // Parse Content-Disposition header to get the original file name
-    private String parseFileName(MultivaluedMap<String, String> headers) {
+    private static String parseFileName(MultivaluedMap<String, String> headers) {
         String[] contentDispositionHeader = headers.getFirst("Content-Disposition").split(";");
         for (String name : contentDispositionHeader) {
             if ((name.trim().startsWith("filename"))) {
@@ -80,18 +74,36 @@ public class GreetingResource {
         return "randomName";
     }
 
-    private void saveFile(InputStream uploadedInputStream, File serverLocation) {
-        try {
-            OutputStream outpuStream = new FileOutputStream(serverLocation);
-            int read = 0;
-            byte[] bytes = new byte[1024];
-            while ((read = uploadedInputStream.read(bytes)) != -1) {
-                outpuStream.write(bytes, 0, read);
+    
+    private static final int MAX_BUFFER_SIZE = Integer.MAX_VALUE - 8;
+    private static final int BUFFER_SIZE = 8192;
+    private static byte[] streamToByte(InputStream source, int contentLength) throws IOException {
+    	int capacity = contentLength; //TODO maybe a more clever algorithm for determining buffer initial size?
+        byte[] buf = new byte[capacity];
+        int nread = 0;
+        int n;
+        for (;;) {
+            // read to EOF which may read more or less than initialSize (eg: file
+            // is truncated while we are reading)
+            while ((n = source.read(buf, nread, capacity - nread)) > 0)
+                nread += n;
+
+            // if last call to source.read() returned -1, we are done
+            // otherwise, try to read one more byte; if that failed we're done too
+            if (n < 0 || (n = source.read()) < 0)
+                break;
+
+            // one more byte was read; need to allocate a larger buffer
+            if (capacity <= MAX_BUFFER_SIZE - capacity) {
+                capacity = Math.max(capacity << 1, BUFFER_SIZE);
+            } else {
+                if (capacity == MAX_BUFFER_SIZE)
+                    throw new OutOfMemoryError("Required array size too large");
+                capacity = MAX_BUFFER_SIZE;
             }
-            outpuStream.flush();
-            outpuStream.close();
-        } catch (IOException e) {
-            e.printStackTrace();
+            buf = Arrays.copyOf(buf, capacity);
+            buf[nread++] = (byte)n;
         }
+        return (capacity == nread) ? buf : Arrays.copyOf(buf, nread);
     }
 }
