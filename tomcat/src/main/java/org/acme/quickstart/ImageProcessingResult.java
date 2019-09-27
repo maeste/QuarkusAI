ef4ea006b9f47f55c739c9dbfaf3f323bba6c1dd
Alessio Soldano <asoldano@redhat.com>
2019-09-28 01:23:49 +0200

    Synch tomcat example; set metadata-complete=true in web.xml to avoid annotation scanning causing Tomcat7 to fail on Java 9 modules-info in JAXB libs

---
+++ tomcat/src/main/java/org/acme/quickstart/ImageProcessingResult.java
@@ -0,0 +1,36 @@
+package org.acme.quickstart;
+
+import java.util.List;
+
+public final class ImageProcessingResult
+{
+   private long processingTime;
+   private List<Probability> probabilities;
+   
+   public ImageProcessingResult(long processingTime, List<Probability> probabilities)
+   {
+      super();
+      this.processingTime = processingTime;
+      this.probabilities = probabilities;
+   }
+   public ImageProcessingResult()
+   {
+      super();
+   }
+   public long getProcessingTime()
+   {
+      return processingTime;
+   }
+   public void setProcessingTime(long processingTime)
+   {
+      this.processingTime = processingTime;
+   }
+   public List<Probability> getProbabilities()
+   {
+      return probabilities;
+   }
+   public void setProbabilities(List<Probability> probabilities)
+   {
+      this.probabilities = probabilities;
+   }
+}
