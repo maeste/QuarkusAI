d56c0296db833c529803715814e0f359081902d6
maeste <stefano.maestri@ironjacamar.org>
2019-09-29 00:26:01 +0200

    porting deeplearning4j to native

---
+++ quarkusNative/src/main/java/org/acme/quickstart/ImageNetLabels.java
@@ -0,0 +1,44 @@
+package org.acme.quickstart;
+
+import org.nd4j.shade.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class ImageNetLabels {
+
+    //FIXME
+    private final static String jsonUrl =
+                    "https://s3.amazonaws.com/deep-learning-models/image-models/imagenet_class_index.json";
+    private static ArrayList<String> predictionLabels = null;
+
+    @SuppressWarnings("unchecked")
+	public static ArrayList<String> getLabels() {
+        if (predictionLabels == null) {
+            HashMap<String, ArrayList<String>> jsonMap;
+            try {
+                jsonMap = new ObjectMapper().readValue(new URL(jsonUrl), HashMap.class);
+                predictionLabels = new ArrayList<>(jsonMap.size());
+                for (int i = 0; i < jsonMap.size(); i++) {
+                    predictionLabels.add(jsonMap.get(String.valueOf(i)).get(1));
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return predictionLabels;
+    }
+
+    /*
+        Returns the description of the nth class in the 1000 classes of ImageNet
+     */
+    public static String getLabel(int n) {
+        if (predictionLabels == null) {
+            getLabels();
+        }
+        return predictionLabels.get(n);
+    }
+
+}
---
+++ quarkusNative/src/main/java/org/acme/quickstart/ImageProcessor.java
@@ -0,0 +1,65 @@
+package org.acme.quickstart;
+
+import org.datavec.image.loader.NativeImageLoader;
+import org.deeplearning4j.nn.graph.ComputationGraph;
+import org.deeplearning4j.zoo.PretrainedType;
+import org.deeplearning4j.zoo.ZooModel;
+import org.deeplearning4j.zoo.model.VGG16;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.dataset.api.preprocessor.DataNormalization;
+import org.nd4j.linalg.dataset.api.preprocessor.VGG16ImagePreProcessor;
+import org.nd4j.linalg.factory.Nd4j;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+public final class ImageProcessor
+{
+   public static List<Probability> labelImage(InputStream imageStream, int numResults) throws Exception
+   {
+	   ZooModel zooModel = VGG16.builder().build();
+	   ComputationGraph vgg16 = (ComputationGraph)zooModel.initPretrained(PretrainedType.IMAGENET);
+	   
+	   // Convert file to INDArray
+	   NativeImageLoader loader = new NativeImageLoader(224, 224, 3);
+       INDArray image = loader.asMatrix(imageStream);
+       
+       // Mean subtraction pre-processing step for VGG
+       DataNormalization scaler = new VGG16ImagePreProcessor();
+       scaler.transform(image);
+
+       //Inference returns array of INDArray, index[0] has the predictions
+       INDArray[] output = vgg16.output(false,image);
+       
+       // convert 1000 length numeric index of probabilities per label
+       // to sorted return top numResult convert to string using helper function VGG16.decodePredictions
+       // "predictions" is string of our results
+       return decodePredictions(output[0], numResults);
+   }
+   
+   private static List<Probability> decodePredictions(INDArray predictions, final int numResults) {
+       ArrayList<String> labels = ImageNetLabels.getLabels();
+       List<Probability> probs = new ArrayList<>(numResults);
+       //brute force collect top numResults
+       int i = 0;
+//       for (int batch = 0; batch < predictions.size(0); batch++) {
+//           predictionDescription += "Predictions for batch ";
+//           if (predictions.size(0) > 1) {
+//               predictionDescription += String.valueOf(batch);
+//           }
+//           predictionDescription += " :";
+       final int batch = 0;
+           INDArray currentBatch = predictions.getRow(batch).dup();
+           while (i < numResults) {
+               final int t = Nd4j.argMax(currentBatch, 1).getInt(0, 0);
+               final float f = currentBatch.getFloat(batch, t);
+               currentBatch.putScalar(0, t, 0);
+               probs.add(new Probability(labels.get(t), f));
+               i++;
+           }
+//       }
+       return probs;
+   }
+
+}
