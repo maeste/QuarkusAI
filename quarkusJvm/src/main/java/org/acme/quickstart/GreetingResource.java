4eccff0a4bbf497e818505f8ac040d4d5a2e73e9
Alessio Soldano <asoldano@redhat.com>
2019-09-28 18:17:47 +0200

    Initial changes for using deeplearning4j - failing in findin opencv_imgproc

--- quarkusJvm/src/main/java/org/acme/quickstart/GreetingResource.java
+++ quarkusJvm/src/main/java/org/acme/quickstart/GreetingResource.java
@@ -26,55 +26,55 @@ public class GreetingResource {
     public ImageProcessingResult loadImage(@HeaderParam("Content-Length") String contentLength, @PathParam("results") int results, MultipartFormDataInput input) throws Exception {
         long before = System.currentTimeMillis();
         InputPart inputPart = input.getFormDataMap().get("file").iterator().next();
-        String fileName = parseFileName(inputPart.getHeaders());
-        byte[] bytes = streamToByte(inputPart.getBody(InputStream.class, null), Integer.parseInt(contentLength));
-        List<Probability> probs = LabelImage.labelImage(fileName, bytes).subList(0, results);
+//        String fileName = parseFileName(inputPart.getHeaders());
+//        byte[] bytes = streamToByte(inputPart.getBody(InputStream.class, null), Integer.parseInt(contentLength));
+        List<Probability> probs = ImageProcessor.labelImage(inputPart.getBody(InputStream.class, null), results);
         return new ImageProcessingResult((System.currentTimeMillis() - before), probs);
     }
 
-    // Parse Content-Disposition header to get the original file name
-    private static String parseFileName(MultivaluedMap<String, String> headers) {
-        String[] contentDispositionHeader = headers.getFirst("Content-Disposition").split(";");
-        for (String name : contentDispositionHeader) {
-            if ((name.trim().startsWith("filename"))) {
-                String[] tmp = name.split("=");
-                String fileName = tmp[1].trim().replaceAll("\"", "");
-                return fileName;
-            }
-        }
-        return "randomName";
-    }
+//    // Parse Content-Disposition header to get the original file name
+//    private static String parseFileName(MultivaluedMap<String, String> headers) {
+//        String[] contentDispositionHeader = headers.getFirst("Content-Disposition").split(";");
+//        for (String name : contentDispositionHeader) {
+//            if ((name.trim().startsWith("filename"))) {
+//                String[] tmp = name.split("=");
+//                String fileName = tmp[1].trim().replaceAll("\"", "");
+//                return fileName;
+//            }
+//        }
+//        return "randomName";
+//    }
 
     
-    private static final int MAX_BUFFER_SIZE = Integer.MAX_VALUE - 8;
-    private static final int BUFFER_SIZE = 8192;
-    private static byte[] streamToByte(InputStream source, int contentLength) throws IOException {
-    	int capacity = contentLength; //TODO maybe a more clever algorithm for determining buffer initial size?
-        byte[] buf = new byte[capacity];
-        int nread = 0;
-        int n;
-        for (;;) {
-            // read to EOF which may read more or less than initialSize (eg: file
-            // is truncated while we are reading)
-            while ((n = source.read(buf, nread, capacity - nread)) > 0)
-                nread += n;
-
-            // if last call to source.read() returned -1, we are done
-            // otherwise, try to read one more byte; if that failed we're done too
-            if (n < 0 || (n = source.read()) < 0)
-                break;
-
-            // one more byte was read; need to allocate a larger buffer
-            if (capacity <= MAX_BUFFER_SIZE - capacity) {
-                capacity = Math.max(capacity << 1, BUFFER_SIZE);
-            } else {
-                if (capacity == MAX_BUFFER_SIZE)
-                    throw new OutOfMemoryError("Required array size too large");
-                capacity = MAX_BUFFER_SIZE;
-            }
-            buf = Arrays.copyOf(buf, capacity);
-            buf[nread++] = (byte)n;
-        }
-        return (capacity == nread) ? buf : Arrays.copyOf(buf, nread);
-    }
+//    private static final int MAX_BUFFER_SIZE = Integer.MAX_VALUE - 8;
+//    private static final int BUFFER_SIZE = 8192;
+//    private static byte[] streamToByte(InputStream source, int contentLength) throws IOException {
+//    	int capacity = contentLength; //TODO maybe a more clever algorithm for determining buffer initial size?
+//        byte[] buf = new byte[capacity];
+//        int nread = 0;
+//        int n;
+//        for (;;) {
+//            // read to EOF which may read more or less than initialSize (eg: file
+//            // is truncated while we are reading)
+//            while ((n = source.read(buf, nread, capacity - nread)) > 0)
+//                nread += n;
+//
+//            // if last call to source.read() returned -1, we are done
+//            // otherwise, try to read one more byte; if that failed we're done too
+//            if (n < 0 || (n = source.read()) < 0)
+//                break;
+//
+//            // one more byte was read; need to allocate a larger buffer
+//            if (capacity <= MAX_BUFFER_SIZE - capacity) {
+//                capacity = Math.max(capacity << 1, BUFFER_SIZE);
+//            } else {
+//                if (capacity == MAX_BUFFER_SIZE)
+//                    throw new OutOfMemoryError("Required array size too large");
+//                capacity = MAX_BUFFER_SIZE;
+//            }
+//            buf = Arrays.copyOf(buf, capacity);
+//            buf[nread++] = (byte)n;
+//        }
+//        return (capacity == nread) ? buf : Arrays.copyOf(buf, nread);
+//    }
 }
