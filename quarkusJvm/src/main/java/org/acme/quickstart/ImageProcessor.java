376f43025c82b26c447342ae9c3751beb8306db2
maeste <stefano.maestri@ironjacamar.org>
2019-10-02 13:05:14 +0200

    DL4J importing .pb

--- quarkusJvm/src/main/java/org/acme/quickstart/ImageProcessor.java
+++ quarkusJvm/src/main/java/org/acme/quickstart/ImageProcessor.java
@@ -1,28 +1,27 @@
 package org.acme.quickstart;
 
+import org.datavec.image.loader.NativeImageLoader;
+import org.nd4j.autodiff.samediff.SameDiff;
+import org.nd4j.imports.graphmapper.tf.TFGraphMapper;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.factory.Nd4j;
+
+import java.io.File;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.datavec.image.loader.NativeImageLoader;
-import org.deeplearning4j.nn.graph.ComputationGraph;
-import org.deeplearning4j.zoo.PretrainedType;
-import org.deeplearning4j.zoo.ZooModel;
-import org.deeplearning4j.zoo.model.VGG16;
-import org.nd4j.linalg.api.ndarray.INDArray;
-import org.nd4j.linalg.dataset.api.preprocessor.DataNormalization;
-import org.nd4j.linalg.dataset.api.preprocessor.VGG16ImagePreProcessor;
-import org.nd4j.linalg.factory.Nd4j;
-
 public final class ImageProcessor
 {
-   private static ComputationGraph vgg16 = null;
+   private static SameDiff sd = null;
+   private static INDArray allZeros;
    static {
 	   try {
-	   @SuppressWarnings("rawtypes")
-	   ZooModel zooModel = VGG16.builder().build();
-	   vgg16 = (ComputationGraph)zooModel.initPretrained(PretrainedType.IMAGENET);
-	   } catch (Exception e) {
+
+	   //@SuppressWarnings("rawtypes")
+       sd = TFGraphMapper.getInstance().importGraph(new File(ImageProcessor.class.getClassLoader().getResource("mobilenet_frozen.pb").getPath()));
+           INDArray allZeros = Nd4j.zeros(1, 128,128,3);
+       } catch (Exception e) {
 		   throw new RuntimeException(e);
 	   }
    }
@@ -30,20 +29,17 @@ public final class ImageProcessor
    public static List<Probability> labelImage(InputStream imageStream, int numResults) throws Exception
    {
 	   // Convert file to INDArray
-	   NativeImageLoader loader = new NativeImageLoader(224, 224, 3);
-       INDArray image = loader.asMatrix(imageStream);
-       
-       // Mean subtraction pre-processing step for VGG
-       DataNormalization scaler = new VGG16ImagePreProcessor();
-       scaler.transform(image);
+	   NativeImageLoader loader = new NativeImageLoader(128, 128, 3);
+       INDArray image = loader.asMatrix(imageStream).permute(0,2,3,1);
 
        //Inference returns array of INDArray, index[0] has the predictions
-       INDArray[] output = vgg16.output(false,image);
+       sd.associateArrayWithVariable(image, sd.variableMap().get("input"));
+       INDArray output = sd.execAndEndResult();
        
        // convert 1000 length numeric index of probabilities per label
        // to sorted return top numResult convert to string using helper function VGG16.decodePredictions
        // "predictions" is string of our results
-       return decodePredictions(output[0], numResults);
+       return decodePredictions(output, numResults);
    }
    
    private static List<Probability> decodePredictions(INDArray predictions, final int numResults) {
--- quarkusNative/src/main/java/org/acme/quickstart/ImageProcessor.java
+++ quarkusNative/src/main/java/org/acme/quickstart/ImageProcessor.java
@@ -1,41 +1,46 @@
 package org.acme.quickstart;
 
 import org.datavec.image.loader.NativeImageLoader;
-import org.deeplearning4j.nn.graph.ComputationGraph;
-import org.deeplearning4j.zoo.PretrainedType;
-import org.deeplearning4j.zoo.ZooModel;
-import org.deeplearning4j.zoo.model.VGG16;
+import org.nd4j.autodiff.samediff.SameDiff;
+import org.nd4j.imports.graphmapper.tf.TFGraphMapper;
 import org.nd4j.linalg.api.ndarray.INDArray;
-import org.nd4j.linalg.dataset.api.preprocessor.DataNormalization;
-import org.nd4j.linalg.dataset.api.preprocessor.VGG16ImagePreProcessor;
 import org.nd4j.linalg.factory.Nd4j;
 
+import java.io.File;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
 public final class ImageProcessor
 {
+   private static SameDiff sd = null;
+   private static INDArray allZeros;
+   private static INDArray output;
+   static {
+	   try {
+
+	   //@SuppressWarnings("rawtypes")
+       sd = TFGraphMapper.getInstance().importGraph(new File(ImageProcessor.class.getClassLoader().getResource("mobilenet_frozen.pb").getPath()));
+           allZeros = Nd4j.zeros(1, 128,128,3);
+           output = Nd4j.zeros(1, 1001);
+       } catch (Exception e) {
+		   throw new RuntimeException(e);
+	   }
+   }
+	
    public static List<Probability> labelImage(InputStream imageStream, int numResults) throws Exception
    {
-	   ZooModel zooModel = VGG16.builder().build();
-	   ComputationGraph vgg16 = (ComputationGraph)zooModel.initPretrained(PretrainedType.IMAGENET);
-	   
 	   // Convert file to INDArray
-	   NativeImageLoader loader = new NativeImageLoader(224, 224, 3);
-       INDArray image = loader.asMatrix(imageStream);
-       
-       // Mean subtraction pre-processing step for VGG
-       DataNormalization scaler = new VGG16ImagePreProcessor();
-       scaler.transform(image);
-
+       long millis = System.currentTimeMillis();
+       NativeImageLoader loader = new NativeImageLoader(128, 128, 3);
+       allZeros = loader.asMatrix(imageStream).permute(0,2,3,1);
        //Inference returns array of INDArray, index[0] has the predictions
-       INDArray[] output = vgg16.output(false,image);
-       
+       sd.associateArrayWithVariable(allZeros, sd.variableMap().get("input"));
+       output = sd.execAndEndResult();
        // convert 1000 length numeric index of probabilities per label
        // to sorted return top numResult convert to string using helper function VGG16.decodePredictions
        // "predictions" is string of our results
-       return decodePredictions(output[0], numResults);
+       return decodePredictions(output, numResults);
    }
    
    private static List<Probability> decodePredictions(INDArray predictions, final int numResults) {
